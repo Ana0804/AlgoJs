760bae152cb982a35bcf3e631d5004a9bf38fdf4
Anaïs Chazelas <anais.chazelas91@gmail.com>
2021-12-15 18:06:52 +0100

    server+var+calcul.js

---
+++ calcul.js
@@ -0,0 +1,27 @@
+{
+    let x = 8;
+    let y = 2;
+    let z = x + y;
+        console.log (z)
+}   
+
+{
+    let x = 5;
+    let y = 4;
+    let z = x - y;
+        console.log (z)
+}
+
+{
+    let x = 5;    
+    let y = 7;    
+    let z = x * y;
+        console.log (z)
+}
+
+{
+    let x = 5;    
+    let y = 2;    
+    let z = x / y;
+        console.log (z)
+}
---
+++ var.js
@@ -0,0 +1,144 @@
+var liquide = 'je bois'; //string
+
+var number = 1; //int
+
+var tasse ='chocolat chaud'; //string
+
+var solide = 'je mange'; //string
+
+var fruit = 'clémentine'; //string
+
+function sayHello(){
+    
+    console.log (liquide + " " + number + " " + tasse + " " + "et" + " " + solide + " " + number + " " + fruit);
+
+}
+
+sayHello();
+
+var price1 = 30;
+var price2 = 22;
+var price3 = 45;
+var total = price1 + price2 + price3;
+
+    console.log (price1 + price2);
+
+var person = "Paul", action = "part travailler", manière = "en vélo", distance = 12, unité = "kilomètres";
+
+    console.log (person + " " + action + " " + manière + " " + distance + unité); 
+
+var $ = 2;
+var $$ = 20;
+var $$$ = 15;
+
+const PI = 3.14159265359;
+    
+    console.log ($+$$+PI);
+
+var pi = 3.14;
+var person = "Mickey Mouse";
+var answer = "quarante-deux";
+
+    console.log("en voyant "+ pi + ", " + person + " eu la réponse à la vie et à tout le reste: " + answer); 
+
+var x = "5" + 2 + 3;
+    console.log (x);
+
+var x = 2 + 3 + "5";
+    console.log (x);
+
+{
+    let x = 8;
+    let y = 2;
+    let z = x + y;
+        console.log (z)
+}   
+
+{
+    let x = 5;
+    let y = 4;
+    let z = x - y;
+        console.log (z)
+}
+
+{
+    let x = 5;    
+    let y = 7;    
+    let z = x * y;
+        console.log (z)
+}
+
+{
+    let x = 5;    
+    let y = 2;    
+    let z = x / y;
+        console.log (z)
+}
+
+{
+    let x = 5;
+    let y = 2;
+    let z = x % y;
+        console.log (z)
+}
+
+var x = 10;
+x += 5;
+    console.log (x)
+
+{
+    let text1 = "Etoile";
+    let text2 = "filante";
+    let text3 = text1 + " " + text2;
+        console.log (text3)
+}
+
+{
+    let text1 = "Ce concert ";
+    text1 += "est génial";
+        console.log (text1)
+}
+
+{
+    let x = 100 + 50
+        console.log (x)
+}
+
+{ 
+    let a = 100;
+    let b = 50;
+    let x = a + b;
+        console.log (x)
+}
+
+{
+    let a = 3;
+    let x = (100 + 50) * a;
+        console.log (x)
+}
+
+{
+    let x = 5;
+    x++;
+    let z = x;
+        console.log (z)
+}
+
+{
+    let x = 5;
+    x--;
+    let z = x;
+        console.log (z)
+}
+
+{
+    let x = 5;
+    let z = x ** 2;
+        console.log (z)
+}
+
+{
+    let x = 5;
+    let z = Math.pow(x,2);
+}
+
